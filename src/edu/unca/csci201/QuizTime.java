05b4d7a26fa72a09b35de31b62d7a6acbc650be9
Robin Phillips <pphilli6@unca.edu>
2024-04-08 08:34:35 -0400

    First build done. Bug fixing time.

--- src/edu/unca/csci201/QuizTime.java
+++ src/edu/unca/csci201/QuizTime.java
@@ -6,22 +6,13 @@ public class QuizTime {
 		
 		Quiz quiz = new Quiz();
 		
-		quiz.addQuestion(null);
-		quiz.addQuestion(null);
-		quiz.addQuestion(null);
-		quiz.addQuestion(null);
-		quiz.addQuestion(null);
-		quiz.addQuestion(null);
-		quiz.addQuestion(null);
-		quiz.addQuestion(null);
-		quiz.addQuestion(null);
 
 		System.out.println("                  <PLACEHOLDER> QUIZ                      \n");
 		
 		System.out.println("        INSTRUCTIONS        ");
-		System.out.println("For multiple choice questions: enter only the letter 'A', 'B', 'C', or 'D'");
-		System.out.println("For true or false questions: enter 't' or 'f'");
-		System.out.println("For fill-in-the blank questions: enter only the word that fills in the blank, no spaces");
+		System.out.println("For multiple choice questions: type only the letter 'A', 'B', 'C', or 'D'");
+		System.out.println("For true or false questions: type only 'T' or 'F'");
+		System.out.println("For fill-in-the blank questions: type your answer");
 		System.out.println();
 		
 		System.out.println("Loading quiz...");
@@ -29,9 +20,19 @@ public class QuizTime {
 		System.out.println("Loading quiz...");
 		System.out.println("QUIZ STARTING NOW... \n");
 		
+		quiz.addQuestion(new TrueFalseQuestion("True or false: The Death Star II was destroyed over Yavin IV by Luke Skywalker", false));
+		quiz.addQuestion(new FillInTheBlankQuestion("What is Mando's real name from The Mandalorian?", "Din Djarin"));
+		quiz.addQuestion(new MultipleChoiceQuestion("What is the name of Han Solo's Ship?\nA. Ebon Hawk\nB. Millenium Falcon\nC. Serenity\nD.Executor", 'B'));
+		quiz.addQuestion(new FillInTheBlankQuestion("Han Solo was frozen in what?", "Carbonite"));
+		quiz.addQuestion(new TrueFalseQuestion("True or False: Darth Vader is Luke Skywalker's father", true));
+		quiz.addQuestion(new MultipleChoiceQuestion("Who created Star Wars?\nA. George Lucas\nB. Steven Spielberg\nC. Denis Villeneuve\nD. Robert Jordan", 'C'));
+		quiz.addQuestion(new FillInTheBlankQuestion("Which character said, \"Help me Obi-Wan Kenobi. You are my only hope\"?", "Princess Leia"));
+		quiz.addQuestion(new MultipleChoiceQuestion("What is Darth Sidious's real name?\nA. Quintus Palpatine\nB. Valkorion\nC. Anakin Skywalker\nD. Sheev Palpatine", 'D'));
+		quiz.addQuestion(new TrueFalseQuestion("True or False: Boba Fett survived the Sarlacc pit", false));
+		
 		double score = quiz.giveQuiz();
 		
-		System.out.println("QUIZ COMPLETE!");
+		System.out.println("\n\nQUIZ COMPLETE!\n");
 		
 		System.out.println("Your score is: " + score + "%!");
 		
